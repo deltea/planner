510cd99c310576cc09a1ec66098c183cbbb3d4a1
deltea <thcheetah777@gmail.com>
2024-12-23 10:35:15 -0700

    better icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,19 @@
   }
 
   const classes: Class[] = [
-    { name: "Math", icon: "mingcute:book-2-fill"},
-    { name: "Spanish", icon: "mingcute:translate-2-line"},
-    { name: "Physics", icon: "mingcute:science-line"},
-    { name: "History", icon: "mingcute:time-fill" },
-    { name: "Robotics", icon: "mingcute:android-2-fill"},
-    { name: "English", icon: "mingcute:speech-fill"},
+    { name: "Math", icon: "mdi:math-compass"},
+    { name: "Spanish", icon: "mdi:translate"},
+    { name: "Physics", icon: "mdi:earth"},
+    { name: "History", icon: "mdi:history" },
+    { name: "Robotics", icon: "mdi:robot"},
+    { name: "English", icon: "mdi:book"},
   ];
 </script>
 
 <div class="p-4 h-full">
   <Section name="nav" class="flex flex-col gap-2 w-20 h-full">
     <button class="bg-background-dark text-foreground hover:bg-primary hover:text-background-1 w-full aspect-square flex justify-center items-center text-2xl duration-default" aria-label="button">
-      <iconify-icon icon="mingcute:home-4-fill"></iconify-icon>
+      <iconify-icon icon="mdi:home"></iconify-icon>
     </button>
 
     <!-- <hr class="border-none h-border w-full bg-background-1"> -->
@@ -32,8 +32,8 @@
     {/each}
 
     <div class="w-full absolute bottom-0 left-0 p-3">
-      <button class="text-foreground hover:bg-primary hover:text-background-1 w-full aspect-square flex justify-center items-center text-2xl duration-default" aria-label="button">
-        <iconify-icon icon="mingcute:add-fill"></iconify-icon>
+      <button class="text-foreground hover:bg-primary hover:text-background-1 w-full aspect-square flex justify-center items-center text-3xl duration-default" aria-label="button">
+        <iconify-icon icon="mdi:plus"></iconify-icon>
       </button>
     </div>
   </Section>
